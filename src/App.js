6735656e5f1d9a88c86f516d31ce4995ac56f41f
Raimo Radczewski <raimoradczewski@googlemail.com>
2018-07-28 14:44:22 +0200

    Add settings dialog

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import store from "store";
+
 import React, { Component } from "react";
 import { Allergies } from "./Allergies";
+import { Settings } from "./Settings.js";
+
 import logo from "./daisy.svg";
+import settings from "./settings.svg";
 
 import IntroText from "./data_intro";
 import "./App.css";
@@ -11,10 +16,19 @@ class App extends Component {
     super(props);
 
     this.state = {
-      allergies: ["kiwi", "cashew", "almond", "pistachio"],
-      languages: ["en", "ar", "zh", "fr", "es"]
+      showSettings: false,
+      allergies: store.get("allergies") || [],
+      languages: store.get("languages") || ["en"]
     };
   }
+
+  updateSettings = ({ allergies, languages }) => {
+    store.set("allergies", allergies);
+    store.set("languages", languages);
+    this.setState({ allergies, languages, showSettings: false });
+  };
+  hideSettings = () => this.setState({ showSettings: false });
+  showSettings = () => this.setState({ showSettings: true });
 
   render() {
     return (
@@ -22,30 +36,101 @@ class App extends Component {
         <div className="TopBar">
           <img alt="Logo for Alergio, a daisy flower" src={logo} id="logo" />
           <h1>Alergio</h1>
+          <button onClick={this.showSettings} id="settings">
+            <img alt="settings" src={settings} />
+          </button>
         </div>
-        <div className="introText">
-          <p className="primaryLanguage">
-            {IntroText[this.state.languages[0]]} <span className="language-code">({this.state.languages[0]})</span>
-          </p>
-          {this.state.languages.slice(1).map(language => (
-            <p key={language} className="otherLanguage">
-              {IntroText[language]} <span className="language-code">({language})</span>
-            </p>
-          ))}
-        </div>
-        <Allergies
-          allergies={this.state.allergies}
-          languages={this.state.languages}
-        />
+        {this.state.showSettings ? (
+          <Settings
+            languages={this.state.languages}
+            allergies={this.state.allergies}
+            hideSettings={this.hideSettings}
+            updateSettings={this.updateSettings}
+          />
+        ) : (
+          <div>
+            <div className="introText">
+              <p className="primaryLanguage">
+                {IntroText[this.state.languages[0]]}{" "}
+                <span className="language-code">
+                  ({this.state.languages[0]})
+                </span>
+              </p>
+              {this.state.languages.slice(1).map(language => (
+                <p key={language} className="otherLanguage">
+                  {IntroText[language]}{" "}
+                  <span className="language-code">({language})</span>
+                </p>
+              ))}
+            </div>
+            <Allergies
+              allergies={this.state.allergies}
+              languages={this.state.languages}
+            />
+          </div>
+        )}
         <div className="footer">
-          <p>Daisy Icon made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">CC 3.0 BY</a></p>
           <p>
-            Tangerine-Regular.ttf: Copyright (c) 2010 by Toshi Omagari. All rights reserved.<br/>
-            Tangerine-Bold.ttf: Copyright (c) 2010 by Toshi Omagari. All rights reserved.
+            Settings Icon made by{" "}
+            <a
+              href="https://www.flaticon.com/authors/smashicons"
+              title="Smashicons"
+            >
+              Smashicons
+            </a>{" "}
+            from{" "}
+            <a href="https://www.flaticon.com/" title="Flaticon">
+              www.flaticon.com
+            </a>{" "}
+            is licensed by{" "}
+            <a
+              href="http://creativecommons.org/licenses/by/3.0/"
+              title="Creative Commons BY 3.0"
+            >
+              CC 3.0 BY
+            </a>
           </p>
-          <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</p>
-          <p><strong>Privacy statement</strong>: We are not storing any of your information. Our hosting is done via netlify, you can read their DPA <a href="https://www.netlify.com/gdpr/">here</a>.</p>
-          <p><a href="https://ymmv.craftswerk.io/imprint/">Imprint</a></p>
+          <p>
+            Daisy Icon made by{" "}
+            <a href="http://www.freepik.com" title="Freepik">
+              Freepik
+            </a>{" "}
+            from{" "}
+            <a href="https://www.flaticon.com/" title="Flaticon">
+              www.flaticon.com
+            </a>{" "}
+            is licensed by{" "}
+            <a
+              href="http://creativecommons.org/licenses/by/3.0/"
+              title="Creative Commons BY 3.0"
+            >
+              CC 3.0 BY
+            </a>
+          </p>
+          <p>
+            Tangerine-Regular.ttf: Copyright (c) 2010 by Toshi Omagari. All
+            rights reserved.<br />
+            Tangerine-Bold.ttf: Copyright (c) 2010 by Toshi Omagari. All rights
+            reserved.
+          </p>
+          <p>
+            THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+            EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+            MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+            NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+            BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+            ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+            CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+            SOFTWARE.
+          </p>
+          <p>
+            <strong>Privacy statement</strong>: We are not storing any of your
+            information. Our hosting is done via netlify, you can read their DPA{" "}
+            <a href="https://www.netlify.com/gdpr/">here</a>.
+          </p>
+          <p>
+            <a href="https://ymmv.craftswerk.io/imprint/">Imprint</a>
+          </p>
         </div>
       </div>
     );
---
+++ src/Settings.js
@@ -0,0 +1,103 @@
+import React from "react";
+import AllergiesDb from "./data_allergies";
+import "./Settings.css";
+
+const availableAllergies = Object.keys(AllergiesDb);
+const availableLanguages = Object.keys(
+  AllergiesDb[availableAllergies[0]].translations
+);
+
+export class Settings extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      allergies: [...props.allergies],
+      languages: [...props.languages]
+    };
+  }
+
+  toggleAllergy = (allergy, checked) => {
+    if (checked) {
+      this.setState({ allergies: [...this.state.allergies, allergy] });
+    } else {
+      this.setState({
+        allergies: this.state.allergies.filter(a => a !== allergy)
+      });
+    }
+  };
+
+  toggleLanguage = (language, checked) => {
+    if (checked) {
+      this.setState({ languages: [...this.state.languages, language] });
+    } else {
+      this.setState({
+        languages: this.state.languages.filter(a => a !== language)
+      });
+    }
+  };
+
+  render() {
+    const { allergies, languages } = this.state;
+    const { updateSettings, hideSettings } = this.props;
+
+    return (
+      <div className="settings">
+        <div className="box">
+          <h2>Allergies</h2>
+          <ul>
+            {availableAllergies.map(allergy => (
+              <li key={allergy}>
+                <input
+                  type="checkbox"
+                  checked={
+                    this.state.allergies.includes(allergy) ? "checked" : ""
+                  }
+                  id={`allergy_${allergy}`}
+                  name="allergy"
+                  onChange={e =>
+                    this.toggleAllergy(e.target.value, e.target.checked)
+                  }
+                  value={allergy}
+                />
+                <label htmlFor={`allergy_${allergy}`}>
+                  {AllergiesDb[allergy].translations.en}
+                </label>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="box">
+          <h2>Languages</h2>
+          <ul>
+            {availableLanguages.map(language => (
+              <li key={language}>
+                <input
+                  type="checkbox"
+                  checked={
+                    this.state.languages.includes(language) ? "checked" : ""
+                  }
+                  id={`language_${language}`}
+                  name="language"
+                  onChange={e =>
+                    this.toggleLanguage(e.target.value, e.target.checked)
+                  }
+                  value={language}
+                />
+                <label htmlFor={`language_${language}`}>{language}</label>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <button className="secondary" onClick={hideSettings}>
+          Cancel
+        </button>&nbsp;
+        <button
+          className="primary"
+          onClick={() => updateSettings({ allergies, languages })}
+        >
+          Save Settings
+        </button>
+      </div>
+    );
+  }
+}
