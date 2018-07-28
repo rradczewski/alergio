8dbfcb3676825a7e16a69a34f01ec4401b38a18f
Raimo Radczewski <raimoradczewski@googlemail.com>
2018-07-28 14:15:45 +0200

    Add Imprint

--- src/App.js
+++ src/App.js
@@ -45,6 +45,7 @@ class App extends Component {
           </p>
           <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</p>
           <p><strong>Privacy statement</strong>: We are not storing any of your information. Our hosting is done via netlify, you can read their DPA <a href="https://www.netlify.com/gdpr/">here</a>.</p>
+          <p><a href="https://ymmv.craftswerk.io/imprint/">Imprint</a></p>
         </div>
       </div>
     );
